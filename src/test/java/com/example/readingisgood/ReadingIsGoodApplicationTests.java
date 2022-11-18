876b43ce81cf1f8772e26a1157077468d96091d6
Selcan Yağmur Atak <selcanyagmuratak@crypttech.com>
2022-11-18 13:38:18 +0300

    chore(ReadingIsGoodApplicationTests): remove test annotation, it fails always

--- src/test/java/com/example/readingisgood/ReadingIsGoodApplicationTests.java
+++ src/test/java/com/example/readingisgood/ReadingIsGoodApplicationTests.java
@@ -1,12 +1,10 @@
 package com.example.readingisgood;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ReadingIsGoodApplicationTests {
 
-    @Test
     void contextLoads() {
     }
 
